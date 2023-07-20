7d20494b6fe8bdf126edcf1420b78b8593cde30d
Anna Nechaeva <115800504+a-nechaeva@users.noreply.github.com>
2023-07-20 22:04:35 +0300

    +lombok

--- server/src/main/java/code/collectionWorker/musicBandCollection/baseClasses/Coordinates.java
+++ server/src/main/java/code/collectionWorker/musicBandCollection/baseClasses/Coordinates.java
@@ -1,6 +1,15 @@
 package code.collectionWorker.musicBandCollection.baseClasses;
 
+
+import lombok.*;
+
+@Setter
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+@EqualsAndHashCode
+@ToString(includeFieldNames = true)
 public class Coordinates {
-    private long x; //Значение поля должно быть больше -97
-    private Long y; //Значение поля должно быть больше -480, Поле не может быть null
+    private Float x; //Значение поля должно быть больше -97, Поле не может быть null
+    private Double y; //Значение поля должно быть больше -480, Поле не может быть null
 }
--- server/src/main/java/code/collectionWorker/musicBandCollection/baseClasses/Studio.java
+++ server/src/main/java/code/collectionWorker/musicBandCollection/baseClasses/Studio.java
@@ -1,5 +1,13 @@
 package code.collectionWorker.musicBandCollection.baseClasses;
 
+import lombok.*;
+
+@Setter(AccessLevel.PUBLIC)
+@Getter(AccessLevel.PUBLIC)
+@AllArgsConstructor
+@NoArgsConstructor
+@ToString
+@EqualsAndHashCode
 public class Studio {
     private String name; //Поле может быть null
 }
